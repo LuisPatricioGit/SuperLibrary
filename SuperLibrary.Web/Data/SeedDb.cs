ffc894a219198240bef204140cd936d9f0fd30b3
LuisPatricioGit <luispatricio.info@gmail.com>
2025-06-09 11:58:51 +0100

    SeedDb

---
+++ SuperLibrary.Web/Data/SeedDb.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SuperLibrary.Web.Data.Entities;
+
+namespace SuperLibrary.Web.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        private Random _random;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+            _random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            if (!_context.Books.Any())
+            {
+                AddBook("DragonBall: Son Goku e os Seus Amigos","Akira Toriyama","Devir");
+                AddBook("Gerónimo Stilton: O Mistério do Olho de Esmeralda","Gerónimo Stilton","Editorial Presença");
+                AddBook("Cherub: O Recruta","Robert Muchamore","Porto Editora");
+                AddBook("Astérix na Lusitânia","René Gosciny","Edições Asa");
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        private void AddBook(string title, string author, string publisher)
+        {
+            _context.Books.Add(new Book
+            {
+                Title = title,
+                Author = author,
+                Publisher = publisher,
+                Copies = _random.Next(1000),
+                GenreId = _random.Next(3),
+                IsAvailable = true
+
+            });
+        }
+    }
+}
