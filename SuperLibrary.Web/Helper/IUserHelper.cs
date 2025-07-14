f689e1b9f75da6c88a1b7019ca1e9252cd8113ce
LuisPatricioGit <luispatricio.info@gmail.com>
2025-07-14 16:00:30 +0100

    Change User Profile and Password

--- SuperLibrary.Web/Helper/IUserHelper.cs
+++ SuperLibrary.Web/Helper/IUserHelper.cs
@@ -14,4 +14,8 @@ public interface IUserHelper
     Task<SignInResult> LoginAsync(LoginViewModel model);
 
     Task LogoutAsync();
+
+    Task<IdentityResult> UpdateUserAsync(User user);
+
+    Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
 }
---
+++ SuperLibrary.Web/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperLibrary.Web.Models;
+
+public class ChangePasswordViewModel
+{
+    [Required]
+    [Display(Name = "Current Password")]
+    [DataType(DataType.Password)]
+    public string OldPassword { get; set; }
+
+    [Required]
+    [Display(Name = "New Password")]
+    [DataType(DataType.Password)]
+    public string NewPassword { get; set; }
+
+    [Required]
+    [Compare("NewPassword")]
+    public string Confirm { get; set; }
+}
